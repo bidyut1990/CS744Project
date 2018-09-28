d614d1894a144eb8e7dd8f8ade4802a3d279bf71
Bidyut <hota@wisc.edu>
2018-09-27 19:26:55 -0500

    PageRank script updated for spark cluster reading from hdfs location

--- scripts/PageRank.py
+++ scripts/PageRank.py
@@ -7,20 +7,14 @@
 import findspark
 from operator import add
 findspark.init()
-from pyspark import SparkConf, SparkContext, SQLContext 
+from pyspark import  SparkConf, SparkContext, SQLContext 
+from pyspark.sql import SparkSession
 
-
-# In[2]:
-
-
-DIR = 'G:/edu/sem3/CS 744/'
-dataDir = DIR+'data/'
-file = dataDir+'web-BerkStan.txt'
-fileOut = dataDir+'ranks.txt'
-
-
-# In[3]:
-
+#DIR = 'G:/edu/sem3/CS 744/'
+#dataDir = DIR+'data/'
+#file = dataDir+'web-BerkStan.txt'
+infile = "hdfs://128.104.222.61:9000/users/Bidyut/web-BerkStan.txt"
+fileOut = 'hdfs://128.104.222.61:9000/users/Bidyut/outputranks.txt'
 
 def flaten(tup):
     _,t = tup # (URL, (List of links, rank))
@@ -28,64 +22,37 @@ def flaten(tup):
     n = len(links)
     return [(dest,rank/n) for dest in links]
 
-
-# In[4]:
-
-
-conf = SparkConf().setMaster("local").setAppName("TableData")
+conf = SparkConf().setMaster("spark://128.104.222.61:7077").setAppName("TableData")
 sc = SparkContext(conf = conf)
 
+data = sc.textFile(infile)
+#sample = data.zipWithIndex().filter(lambda x:x[1]<1000).map(lambda l:l[0])
 
-# In[5]:
-
-
-data = sc.textFile(file)
-
-
-# In[6]:
-
-
-sample = data.zipWithIndex().filter(lambda x:x[1]<1000).map(lambda l:l[0])
-
-
-# In[7]:
-
-
-links = sample.filter(lambda x: x[0]!='#').map(lambda x: x.split('\t')).groupBy(lambda l: l[0]).map(lambda x : (x[0], [l[1] for l in list(x[1])]))
+links = data.filter(lambda x: x[0]!='#').map(lambda x: x.split('\t')).groupBy(lambda l: l[0]).map(lambda x : (x[0], [l[1] for l in list(x[1])]))
 links.persist()
 
-
-# In[8]:
-
-
 N = links.count()
-r0 = 0.25
+r0 = 1.0
 iterations = 10
-a = 0.25
-
-
-# In[9]:
-
+a = 0.85
 
 ranks = links.keys().map(lambda x: (x,r0))
 
-
-# In[10]:
-
-
 for i in range(iterations):
     contribs = links.join(ranks).flatMap(flaten)
-    ranks = contribs.reduceByKey(add).mapValues(lambda x: a/N + (1-a)*x)
-
-
-# In[11]:
-
+    #ranks = contribs.reduceByKey(add).mapValues(lambda x: a/N + (1-a)*x)
+    #Tweaking the values acc. to  the assignment details
+    ranks = contribs.reduceByKey(add).mapValues(lambda x: 0.15 + (0.85)*x)
 
 out = ranks.collect()
+sparkSession = SparkSession.builder.appName("pagerank").getOrCreate()
+df = sparkSession.createDataFrame(out)
+df.coalesce(1).write.csv(fileOut)
+
+#f = open(fileOut, "w+")
+#f.write(str(out))
+#f.close()
 
 
-# In[13]:
-
-
-[l for l in out if l[0]=='1']
+#[l for l in out if l[0]=='1']
 
