343a4d4a2fc189f053430da01bd1234e642b2467
Bidyut <hota@wisc.edu>
2018-10-01 22:46:19 -0500

    Four part file  submissions

--- scripts/PageRank_part3_task3.py
+++ scripts/PageRank_part3_task3.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-#import findspark
+import sys
 from operator import add
-#findspark.init()
-from pyspark import  SparkConf, SparkContext
+from pyspark import  SparkConf, SparkContext, SQLContext 
 from pyspark.sql import SparkSession
 
+#infile = "hdfs://128.104.222.61:9000/CS744/inputdata/enwiki-pages-articles/link-enwiki-20180601-pages-articles14.xml-p7697599p7744799"
 infile = "hdfs://128.104.222.61:9000/CS744/inputdata/enwiki-pages-articles/link-enwiki-20180601-pages-articles*"
-fileOut = 'hdfs://128.104.222.61:9000/CS744/outputdata/outputranks_bigsample1.txt'
+fileOut = 'hdfs://128.104.222.61:9000/CS744/outputdata/outputranks_bigsample.txt'
 
 def flaten(tup):
     key,value = tup # (URL, (List of links, rank))
@@ -20,19 +19,24 @@ def flaten(tup):
 
 conf = SparkConf().setMaster("spark://128.104.222.61:7077").setAppName("TableData")
 sc = SparkContext(conf = conf)
+noofpartitions = 120
+
+def partitioner(key):
+	return hash(key)
 
 data = sc.textFile(infile)
-#sample = data.zipWithIndex().filter(lambda x:x[1]<1000).map(lambda l:l[0])
 
 links = data.map(lambda x: x.split('\t'))#break line
 links = links.filter(lambda l: ':' not in l[1] or l[1][0:9] == 'Category:')#ignore some
 links = links.map(lambda l: [k.lower() for k in l])#to lowercase
 links = links.groupBy(lambda l: l[0])#group by key as from value
 links = links.map(lambda x : (x[0], [l[1] for l in list(x[1])]))#convert iterator to list of values
+print("No of partitions of links RDD before custom partition: {}").format(links.getNumPartitions())
+links = links.partitionBy(noofpartitions,partitioner) 
+print("No of partitions of links RDD after custom partition: {}").format(links.getNumPartitions())
 
-#links.cache()
+links.cache()#saving as in-memory objects
 
-#N = links.count()
 r0 = 1.0
 iterations = 10
 
@@ -42,5 +46,5 @@ for i in range(iterations):
     contribs = links.join(ranks).flatMap(flaten)
     ranks = contribs.reduceByKey(add).mapValues(lambda x: 0.15 + (0.85)*x)
 
-#out = ranks.collect()
 ranks.saveAsNewAPIHadoopFile(fileOut, "org.apache.hadoop.mapreduce.lib.output.TextOutputFormat","org.apache.hadoop.io.Text","org.apache.hadoop.io.Text")
+
