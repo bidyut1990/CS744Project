ff0ed495124972531161e5a6bab29b223aaaf797
Yudhister Satija <satyudhi@gmail.com>
2018-10-12 23:17:06 -0500

    bathed processing

--- Assignment2/LR/code_template.py
+++ Assignment2/LR/code_template.py
@@ -14,37 +14,37 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 tf.logging.set_verbosity(tf.logging.DEBUG)
 
 clusterSpec_single = tf.train.ClusterSpec({
-    "worker" : [
-        "10.10.1.2:2222"
-    ]
+	"worker" : [
+		"10.10.1.2:2222"
+	]
 })
 
 clusterSpec_cluster = tf.train.ClusterSpec({
-    "ps" : [
-        "10.10.1.2:3333"
-    ],
-    "worker" : [
-        "10.10.1.2:2222",
-        "10.10.1.4:2222"
-    ]
+	"ps" : [
+		"10.10.1.2:3333"
+	],
+	"worker" : [
+		"10.10.1.2:2222",
+		"10.10.1.4:2222"
+	]
 })
 
 clusterSpec_cluster2 = tf.train.ClusterSpec({
-    "ps" : [
-        "10.10.1.2:3333"
-    ],
-    "worker" : [
-        "10.10.1.2:2222",
-        "10.10.1.4:2222",
-        "10.10.1.3:2222",
-        "10.10.1.1:2222"
-    ]
+	"ps" : [
+		"10.10.1.2:3333"
+	],
+	"worker" : [
+		"10.10.1.2:2222",
+		"10.10.1.4:2222",
+		"10.10.1.3:2222",
+		"10.10.1.1:2222"
+	]
 })
 
 clusterSpec = {
-    "single": clusterSpec_single,
-    "cluster": clusterSpec_cluster,
-    "cluster2": clusterSpec_cluster2
+	"single": clusterSpec_single,
+	"cluster": clusterSpec_cluster,
+	"cluster2": clusterSpec_cluster2
 }
 
 
@@ -58,6 +58,14 @@ if FLAGS.job_name == "ps":
 	server.join()
 elif FLAGS.job_name == "worker":
 	#put your code here
+	train_X, train_y = (mnist.train.images,mnist.train.labels)
+	test_X, test_y = (mnist.test.images,mnist.test.labels)
+	classCount = train_y.shape[1]
+	featureCount = train_X.shape[1]
+	trainSize = train_X.shape[0]
+	print("Num features:",featureCount,"Num classes:", classCount)
+	print("Train size:",trainSize)
+	
 	W = tf.Variable(tf.random_normal(shape=[featureCount, classCount]))
 	b = tf.Variable(tf.random_normal(shape=[1, classCount]))	
 	
@@ -72,50 +80,38 @@ elif FLAGS.job_name == "worker":
 	loss = tf.reduce_mean(-tf.reduce_sum(target*tf.log(prediction), reduction_indices=1))
 
 	learning_rate = 0.5
-	#batch_size = 30
-	#iter_num = 15
-
-
-	train_X, train_y = (mnist.train.images,mnist.train.labels)
-	test_X, test_y = (mnist.test.images,mnist.test.labels)
-    classCount = train_y.shape[1]
-    featureCount = train_X.shape[1]
-    trainSize = train_X.shape[0]
-    print("Num features:",featureCount,"Num classes:", classCount)
-    print("Train size:",trainSize)
+	batch_size = int(trainSize*0.01)# 1% of examples
+	num_batches = 100
+	iter_num = 1
+	
 	opt = tf.train.GradientDescentOptimizer(learning_rate)
-
 	goal = opt.minimize(loss)
 
-	#prediction = tf.round(tf.sigmoid(mod))
-	# Bool into float32 type
 	correct = tf.cast(tf.equal(tf.round(prediction), target), dtype=tf.float32)
-	# Average
 	accuracy = tf.reduce_mean(correct)
 
 	#loss_trace = []
 	#train_acc = []
 	#test_acc = []
-
-	#for epoch in range(iter_num):
-    for index in range(trainSize):
-		# Generate random batch index
-		#batch_index = np.random.choice(len(train_X), size=batch_size)
-		#batch_train_X = train_X[batch_index]
-		#batch_train_y = np.matrix(train_y[batch_index])
-        x,y = train_X[index], train_y[index]
-		sess.run(goal, feed_dict={data: [x], target: [y]})
-		#temp_loss = sess.run(loss, feed_dict={data: batch_train_X, target: batch_train_y})
-		# convert into a matrix, and the shape of the placeholder to correspond
-		#temp_train_acc = sess.run(accuracy, feed_dict={data: train_X, target: np.matrix(train_y)})
-		#temp_test_acc = sess.run(accuracy, feed_dict={data: test_X, target: np.matrix(test_y)})
-		# recode the result
-		#loss_trace.append(temp_loss)
-		#train_acc.append(temp_train_acc)
-		#test_acc.append(temp_test_acc)
-		# output
-		#if (epoch + 1) % 3 == 0:
-		#	print('epoch: {:4d} loss: {:5f} train_acc: {:5f} test_acc: {:5f}'.format(epoch + 1, temp_loss,
-		#		temp_train_acc, temp_test_acc))
-    test_acc = sess.run(accuracy, feed_dict={data: test_X, target: test_y})
-    print("Test Set accuracy: ",test_acc)
+	for epoch in range(iter_num):
+		for index in range(num_batches)
+			# Generate continuos batch index as images already in random seq
+			batch = range(index*batch_size, min(trainSize, (index+1)*batch_size))
+			batch_X,batch_y = train_X[batch], train_y[batch]
+			sess.run(goal, feed_dict={data: batch_X, target: batch_y})
+			#temp_loss = sess.run(loss, feed_dict={data: batch_train_X, target: batch_train_y})
+			# convert into a matrix, and the shape of the placeholder to correspond
+			#temp_train_acc = sess.run(accuracy, feed_dict={data: train_X, target: np.matrix(train_y)})
+			#temp_test_acc = sess.run(accuracy, feed_dict={data: test_X, target: np.matrix(test_y)})
+			# recode the result
+			#loss_trace.append(temp_loss)
+			#train_acc.append(temp_train_acc)
+			#test_acc.append(temp_test_acc)
+			# output
+			#if (epoch + 1) % 3 == 0:
+			#	print('epoch: {:4d} loss: {:5f} train_acc: {:5f} test_acc: {:5f}'.format(epoch + 1, temp_loss,
+			#		temp_train_acc, temp_test_acc))
+	test_acc = sess.run(accuracy, feed_dict={data: test_X, target: test_y})
+	test_loss = sess.run(loss, feed_dict={data: test_X, target: test_y})
+	print("Test Set accuracy: ",test_acc)
+	print("Test Set loss: ",test_loss)
