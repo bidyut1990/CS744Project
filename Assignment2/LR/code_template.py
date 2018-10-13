c7226c1ea1d162b14905b1ee02ec837e8839a541
Yudhister Satija <satyudhi@gmail.com>
2018-10-12 23:41:17 -0500

    repeat until convergence, save points

--- Assignment2/LR/code_template.py
+++ Assignment2/LR/code_template.py
@@ -80,8 +80,8 @@ elif FLAGS.job_name == "worker":
 	loss = tf.reduce_mean(-tf.reduce_sum(target*tf.log(prediction), reduction_indices=1))
 
 	learning_rate = 0.5
-	batch_size = int(trainSize*0.01)# 1% of examples
 	num_batches = 100
+	batch_size = int(trainSize/num_batches)# 1% of examples
 	iter_num = 1
 	
 	opt = tf.train.GradientDescentOptimizer(learning_rate)
@@ -90,28 +90,31 @@ elif FLAGS.job_name == "worker":
 	correct = tf.cast(tf.equal(tf.round(prediction), target), dtype=tf.float32)
 	accuracy = tf.reduce_mean(correct)
 
-	#loss_trace = []
 	#train_acc = []
-	#test_acc = []
-	for epoch in range(iter_num):
-		for index in range(num_batches)
+	test_acc = [sess.run(accuracy, feed_dict={data: test_X, target: test_y})]
+	prev_loss = np.inf
+	test_loss = sess.run(loss, feed_dict={data: test_X, target: test_y})
+	loss_trace = [test_loss]
+	epsilon = 1
+	epoch = 0
+	
+	#Run until convergence
+	while abs(prev_loss - test_loss)>epsilon:
+		#TODO randomize data
+		for index in range(num_batches):
 			# Generate continuos batch index as images already in random seq
 			batch = range(index*batch_size, min(trainSize, (index+1)*batch_size))
 			batch_X,batch_y = train_X[batch], train_y[batch]
+			# optimize on this batch
 			sess.run(goal, feed_dict={data: batch_X, target: batch_y})
-			#temp_loss = sess.run(loss, feed_dict={data: batch_train_X, target: batch_train_y})
-			# convert into a matrix, and the shape of the placeholder to correspond
-			#temp_train_acc = sess.run(accuracy, feed_dict={data: train_X, target: np.matrix(train_y)})
-			#temp_test_acc = sess.run(accuracy, feed_dict={data: test_X, target: np.matrix(test_y)})
-			# recode the result
-			#loss_trace.append(temp_loss)
-			#train_acc.append(temp_train_acc)
-			#test_acc.append(temp_test_acc)
-			# output
-			#if (epoch + 1) % 3 == 0:
-			#	print('epoch: {:4d} loss: {:5f} train_acc: {:5f} test_acc: {:5f}'.format(epoch + 1, temp_loss,
-			#		temp_train_acc, temp_test_acc))
-	test_acc = sess.run(accuracy, feed_dict={data: test_X, target: test_y})
-	test_loss = sess.run(loss, feed_dict={data: test_X, target: test_y})
-	print("Test Set accuracy: ",test_acc)
-	print("Test Set loss: ",test_loss)
+			# make measurements on test set
+			prev_loss = test_loss
+			test_loss = sess.run(loss, feed_dict={data: test_X, target: test_y})
+			test_acc.append(sess.run(accuracy, feed_dict={data: test_X, target: test_y}))
+			loss_trace.append(test_loss)
+		epoch = epoch+1
+			
+	
+	print("final Test Set accuracy: ",test_acc[-1])
+	print("final Test Set loss: ",test_loss)
+	print("Total epochs: ",epoch)
